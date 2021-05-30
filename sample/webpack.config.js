654e44623d5c88369f46ae6d821f8e61d2b13bfd
ssola129 <ssola129@gmail.com>
2021-05-31 00:29:57 +0900

    feat: webpack 심화 과정 실습

--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -1,67 +1,76 @@
-const path = require('path')
-const webpack = require('webpack')
-const childProcess = require('child_process')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
-const MiniCssExtreactPlugin = require('mini-css-extract-plugin')
+const path = require("path");
+const webpack = require("webpack");
+const childProcess = require("child_process");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtreactPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    main: './src/app.js'
+    main: "./src/app.js",
   },
   output: {
-    path: path.resolve('./dist'),
-    filename: '[name].js'
+    path: path.resolve("./dist"),
+    filename: "[name].js",
+  },
+  devServer: {
+    overlay: true,
+    stats: "errors-only",
   },
   module: {
     rules: [
       {
         test: /\.css$/,
         use: [
-          process.env.NODE_ENV === 'production'
-          ? MiniCssExtreactPlugin.loader
-          : 'style-loader',
-          'css-loader'
-        ]
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtreactPlugin.loader
+            : "style-loader",
+          "css-loader",
+        ],
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
-        loader: 'url-loader',
+        loader: "url-loader",
         options: {
           // publicPath: './dist/',
-          name: '[name].[ext]?[hash]',
+          name: "[name].[ext]?[hash]",
           limit: 20000, //2kb
-        }
-      }
-    ]
+        },
+      },
+    ],
   },
   plugins: [
     new webpack.BannerPlugin({
       banner: `
         Build Date: ${new Date().toLocaleDateString()}
-        Commit Version: ${childProcess.execSync('git rev-parse --short HEAD')}
-        Author: ${childProcess.execSync('git config user.name')}
-      `
+        Commit Version: ${childProcess.execSync("git rev-parse --short HEAD")}
+        Author: ${childProcess.execSync("git config user.name")}
+      `,
     }),
     new webpack.DefinePlugin({
-      TWO: JSON.stringify('1+1')
+      TWO: JSON.stringify("1+1"),
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === 'development' ? '(개발용)' : ''
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : "",
       },
-      minify: process.env.NODE_ENV === 'prodution' ? {
-        collapseWhitespace: true,
-        removeComments: true
-      } : false
+      minify:
+        process.env.NODE_ENV === "prodution"
+          ? {
+              collapseWhitespace: true,
+              removeComments: true,
+            }
+          : false,
     }),
     new CleanWebpackPlugin(),
-    ...(process.env.NODE_ENV === 'production' ? [
-      new MiniCssExtreactPlugin({
-        filename: '[name].css'
-      })
-    ] : [])
-  ]
-}
+    ...(process.env.NODE_ENV === "production"
+      ? [
+          new MiniCssExtreactPlugin({
+            filename: "[name].css",
+          }),
+        ]
+      : []),
+  ],
+};
